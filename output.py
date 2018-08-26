374fb94feab1bfbdc3ab72c9004920b59198d9a5
Nicholas Lee <csudcy@gmail.com>
2018-08-26 08:36:20 +0100

    Output all years at once, add links between full/compact lineups

--- output.py
+++ output.py
@@ -2,13 +2,7 @@ import codecs
 import json
 import os
 
-YEAR = 2018
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-YEAR_DIRECTORY = os.path.join(CURRENT_DIRECTORY, str(YEAR))
-SOURCE = os.path.join(YEAR_DIRECTORY, 'lineup.json')
-DEST_FULL = os.path.join(YEAR_DIRECTORY, 'lineup.html')
-DEST_SMALL = os.path.join(YEAR_DIRECTORY, 'lineup_small.html')
-PREFS_SOURCE = os.path.join(YEAR_DIRECTORY, 'prefs.json')
 
 HOUR_MIN = 10
 HOUR_MAX = 24
@@ -18,7 +12,7 @@ HOUR_WIDTH = 50
 MINUTE_WIDTH = float(HOUR_WIDTH) / 60.0
 DAY_WIDTH = (HOUR_MAX - HOUR_MIN) * HOUR_WIDTH
 
-# Small table
+# Compact table
 HOUR_HEIGHT = 75
 MINUTE_HEIGHT = float(HOUR_HEIGHT) / 60.0
 DAY_HEIGHT = (HOUR_MAX - HOUR_MIN) * HOUR_HEIGHT
@@ -28,27 +22,24 @@ INCLUDED_STAGES = [
   'CASTLE STAGE',
 ]
 
-print 'Reading {SOURCE}...'.format(SOURCE=SOURCE)
-with open(SOURCE, 'r') as f:
-  DAYS = json.load(f)
 
-if os.path.exists(PREFS_SOURCE):
-  with open(PREFS_SOURCE, 'r') as f:
-    PREFS = json.load(f)
-else:
-  PREFS = {}
+###############################
+# Utility functions
+###############################
 
 def hm_to_minutes(t):
   h, m = t.split(':')
   return int(h) * 60 + int(m)
 
+
 def minutes_to_hm(hour, minute):
   return '{h:02}:{m:02}'.format(h=hour, m=minute)
 
-def get_act_style(act):
+
+def get_act_style(act, prefs):
   prefs = {
     person: score
-    for person, person_prefs in PREFS.items()
+    for person, person_prefs in prefs.items()
     for pref_act, score in person_prefs.items()
     if pref_act == act['artist']
   }
@@ -57,6 +48,7 @@ def get_act_style(act):
 
   max_score = max(prefs.values())
   return 'background-color: rgba(255, 0, 255, {a});'.format(a=float(max_score)/5.0)
+
 
 def get_time_div(time_from, time_to, text, style=None):
   minutes_from = hm_to_minutes(time_from)
@@ -71,7 +63,8 @@ def get_time_div(time_from, time_to, text, style=None):
     style=style,
   )
 
-def get_time_div_small(time_from, time_to, text, style=None):
+
+def get_time_div_compact(time_from, time_to, text, style=None):
   minutes_from = hm_to_minutes(time_from)
   minutes_to = hm_to_minutes(time_to)
   top = (minutes_from - (HOUR_MIN * 60)) * MINUTE_HEIGHT
@@ -85,64 +78,92 @@ def get_time_div_small(time_from, time_to, text, style=None):
     style=style,
   )
 
+
 def save(filename, content):
   print 'Writing {filename}...'.format(filename=filename)
   content = content.replace('><', '>\n<')
   with codecs.open(filename, 'w', 'utf-8') as f:
     f.write(content)
 
-def output_full():
-  print 'Processing (full)...'
-  OUTPUT = u'<html><head><title>Victorious {YEAR}</title></head><body>'.format(YEAR=YEAR)
 
-  for day in DAYS:
+###############################
+# Year based functions
+###############################
+
+def load_lineup(year):
+  print 'Reading {year} lineup...'.format(year=year)
+  filename = os.path.join(CURRENT_DIRECTORY, str(year), 'lineup.json')
+  with open(filename, 'r') as f:
+    return json.load(f)
+
+
+def load_prefs(year):
+  print 'Reading {year} prefs...'.format(year=year)
+  filename = os.path.join(CURRENT_DIRECTORY, str(year), 'prefs.json')
+  if not os.path.exists(filename):
+    return {}
+  with open(filename, 'r') as f:
+    return json.load(f)
+
+
+def output_full(year, prefs, lineup):
+  print 'Processing (full)...'
+  output = u'<html><head><title>Victorious {year}</title></head><body>'.format(year=year)
+  output += '<a href="../">&lt;-- Index</a>&nbsp;&nbsp;&nbsp;&nbsp;'
+  output += '<a href="lineup_compact.html">Compact Lineup</a>'
+
+  for day in lineup:
     # Add the headers
-    OUTPUT += u'<h1>{day}</h1><table border="1"><thead><tr><td>Stage</td>'.format(day=day['day'])
-    OUTPUT += u'<td style="position: relative; height: 22px; min-width: {w}px;">'.format(w=DAY_WIDTH)
+    output += u'<h1>{day}</h1><table border="1"><thead><tr><td>Stage</td>'.format(day=day['day'])
+    output += u'<td style="position: relative; height: 22px; min-width: {w}px;">'.format(w=DAY_WIDTH)
     for hour in xrange(HOUR_MIN, HOUR_MAX):
       time_from = minutes_to_hm(hour, 0)
       time_to = minutes_to_hm(hour + 1, 0)
-      OUTPUT += get_time_div(time_from, time_to, time_from)
-    OUTPUT += u'</td></tr></thead><tbody>'
+      output += get_time_div(time_from, time_to, time_from)
+    output += u'</td></tr></thead><tbody>'
 
     # Add the body
     for stage in day['stages']:
-      OUTPUT += u'<tr><td style="white-space: nowrap;">{stage}</td>'.format(stage=stage['stage'])
-      OUTPUT += u'<td style="position: relative; height: 22px;">'
+      output += u'<tr><td style="white-space: nowrap;">{stage}</td>'.format(stage=stage['stage'])
+      output += u'<td style="position: relative; height: 22px;">'
       for act in stage['acts']:
         text = u'{artist} ({time_from} - {time_to})'.format(**act)
-        style = get_act_style(act)
-        OUTPUT += get_time_div(act['time_from'], act['time_to'], text, style=style)
-      OUTPUT += u'</td>'
-      OUTPUT += u'</tr>'
+        style = get_act_style(act, prefs)
+        output += get_time_div(act['time_from'], act['time_to'], text, style=style)
+      output += u'</td>'
+      output += u'</tr>'
 
     # Close up
-    OUTPUT += u'</tbody></table></body></html>'
+    output += u'</tbody></table></body></html>'
 
-  save(DEST_FULL, OUTPUT)
+  filename = os.path.join(CURRENT_DIRECTORY, str(year), 'lineup.html')
+  save(filename, output)
 
-def output_small():
-  print 'Processing (small)...'
-  OUTPUT = u'<html><head><title>Victorious {YEAR}</title></head><body>'.format(YEAR=YEAR)
 
-  OUTPUT += u'<table border="1" style="width: 100%;"><thead><tr><th>Time</th>'
+def output_compact(year, prefs, lineup):
+  print 'Processing (compact)...'
+  output = u'<html><head><title>Victorious {year}</title></head><body>'.format(year=year)
+  output += '<a href="../">&lt;-- Index</a>&nbsp;&nbsp;&nbsp;&nbsp;'
+  output += '<a href="lineup.html">Full Lineup</a>'
+
+  output += u'<table border="1" style="width: 100%;"><thead><tr><th>Time</th>'
   for stage in INCLUDED_STAGES:
-    OUTPUT += u'<th>{stage}</th>'.format(stage=stage)
-  OUTPUT += u'</tr></thead><tbody>'
+    output += u'<th>{stage}</th>'.format(stage=stage)
+  output += u'</tr></thead><tbody>'
 
 
-  for day in DAYS:
+  for day in lineup:
     # Add the headers
-    OUTPUT += u'<tr><td colspan="{cols}" style="font-size: 2em; font-weight: bold; text-align:center;">{day}</td></tr>'.format(
+    output += u'<tr><td colspan="{cols}" style="font-size: 2em; font-weight: bold; text-align:center;">{day}</td></tr>'.format(
       cols=len(INCLUDED_STAGES)+1, day=day['day'])
 
     # Add the time column
-    OUTPUT += u'<td style="position: relative; height: {height}px;">'.format(height=DAY_HEIGHT)
+    output += u'<td style="position: relative; height: {height}px;">'.format(height=DAY_HEIGHT)
     for hour in xrange(HOUR_MIN, HOUR_MAX):
       time_from = minutes_to_hm(hour, 0)
       time_to = minutes_to_hm(hour + 1, 0)
-      OUTPUT += get_time_div_small(time_from, time_to, time_from)
-    OUTPUT += u'</td>'
+      output += get_time_div_compact(time_from, time_to, time_from)
+    output += u'</td>'
 
     # Add each stage column
     for stage_name in INCLUDED_STAGES:
@@ -155,25 +176,35 @@ def output_small():
 
       # Check it is on today
       if not stages:
-        OUTPUT += '<td>&nbsp;</td>'
+        output += '<td>&nbsp;</td>'
         continue
 
       stage = stages[0]
 
       # Add the stage column
-      OUTPUT += u'<td style="position: relative; height: {height}px;">'.format(height=DAY_HEIGHT)
+      output += u'<td style="position: relative; height: {height}px;">'.format(height=DAY_HEIGHT)
       for act in stage['acts']:
         text = u'{artist}<br/>{time_from} - {time_to}'.format(**act)
-        style = get_act_style(act)
-        OUTPUT += get_time_div_small(act['time_from'], act['time_to'], text, style=style)
-      OUTPUT += u'</td>'
+        style = get_act_style(act, prefs)
+        output += get_time_div_compact(act['time_from'], act['time_to'], text, style=style)
+      output += u'</td>'
 
-    OUTPUT += u'</tr>'
+    output += u'</tr>'
 
   # Close up
-  OUTPUT += u'</tbody></table></body></html>'
+  output += u'</tbody></table></body></html>'
 
-  save(DEST_SMALL, OUTPUT)
+  filename = os.path.join(CURRENT_DIRECTORY, str(year), 'lineup_compact.html')
+  save(filename, output)
 
-output_full()
-output_small()
+
+def process_year(year):
+  lineup = load_lineup(year)
+  prefs = load_prefs(year)
+  output_full(year, prefs, lineup)
+  output_compact(year, prefs, lineup)
+
+
+if __name__ == '__main__':
+  process_year(2017)
+  process_year(2018)
